5d0278442ab071d73d7ad539c335e786e585228a
Anton Antonov <anton_player94@ukr.net>
2022-05-28 04:06:12 +0300

    first

---
+++ DoWhileStatments/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+using System;
+
+namespace DoWhileStatments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = String.Empty;
+
+            do
+            {
+                Write("Enter password: ");
+                password = ReadLine();
+            } while (password != "password");
+
+            WriteLine("Correct!");
+
+            ReadKey();
+        }
+    }
+}
---
+++ WhileStatments/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+namespace WhileStatments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+
+            while (x < 10)
+            {
+                ++x;
+            }
+
+            WriteLine(x);
+
+            ReadKey();
+        }
+    }
+}
